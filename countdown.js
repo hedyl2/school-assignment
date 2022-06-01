b2bb7928a1fcf60fb70f1d946c9265ff313c7f04
hedyl2 <100638849+hedyl2@users.noreply.github.com>
2022-06-01 04:35:46 -0700

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,55 +1,55 @@
 function timerdisplayv1(){
 	console.log("timerdisplayv1() begin");
-	var counter = 20;
+	var counter = 50;
     document.getElementById("timerdisplay").innerHTML = counter;
     console.log(counter);
   setTimeout(function(){
-    counter = counter - 2;
+    counter = counter - 5;
     document.getElementById("timerdisplay").innerHTML = counter;
     console.log(counter);
   }, 2000);
   setTimeout(function(){
-    counter = counter - 2;
+    counter = counter - 5;
     document.getElementById("timerdisplay").innerHTML = counter;
     console.log(counter);
   }, 4000);
   setTimeout(function(){
-    counter = counter - 2;
+    counter = counter - 5;
     document.getElementById("timerdisplay").innerHTML = counter;
     console.log(counter);
   }, 6000);
   setTimeout(function(){
-    counter = counter - 2;
+    counter = counter - 5;
     document.getElementById("timerdisplay").innerHTML = counter;
     console.log(counter);
   }, 8000);
   setTimeout(function(){
-    counter = counter - 2;
+    counter = counter - 5;
     document.getElementById("timerdisplay").innerHTML = counter;
     console.log(counter);
   }, 10000);
   setTimeout(function(){
-    counter = counter - 2;
+    counter = counter - 5;
     document.getElementById("timerdisplay").innerHTML = counter;
     console.log(counter);
   }, 12000);
   setTimeout(function(){
-    counter = counter - 2;
+    counter = counter - 5;
     document.getElementById("timerdisplay").innerHTML = counter;
     console.log(counter);
   }, 14000);
   setTimeout(function(){
-    counter = counter - 2;
+    counter = counter - 5;
     document.getElementById("timerdisplay").innerHTML = counter;
     console.log(counter);
   }, 16000);
   setTimeout(function(){
-    counter = counter - 2;
+    counter = counter - 5;
     document.getElementById("timerdisplay").innerHTML = counter;
     console.log(counter);
   }, 18000);
   setTimeout(function(){
-    counter = counter - 2;
+    counter = counter - 5;
     document.getElementById("timerdisplay").innerHTML = "Fire!";
     console.log(counter);
   }, 20000);
