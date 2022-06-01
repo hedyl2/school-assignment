f227ea26555f5033b4627f97a273ce46ede57871
hedyl2 <100638849+hedyl2@users.noreply.github.com>
2022-06-01 04:33:33 -0700

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,56 @@
+function timerdisplayv1(){
+	console.log("timerdisplayv1() begin");
+	var counter = 20;
+    document.getElementById("timerdisplay").innerHTML = counter;
+    console.log(counter);
+  setTimeout(function(){
+    counter = counter - 2;
+    document.getElementById("timerdisplay").innerHTML = counter;
+    console.log(counter);
+  }, 2000);
+  setTimeout(function(){
+    counter = counter - 2;
+    document.getElementById("timerdisplay").innerHTML = counter;
+    console.log(counter);
+  }, 4000);
+  setTimeout(function(){
+    counter = counter - 2;
+    document.getElementById("timerdisplay").innerHTML = counter;
+    console.log(counter);
+  }, 6000);
+  setTimeout(function(){
+    counter = counter - 2;
+    document.getElementById("timerdisplay").innerHTML = counter;
+    console.log(counter);
+  }, 8000);
+  setTimeout(function(){
+    counter = counter - 2;
+    document.getElementById("timerdisplay").innerHTML = counter;
+    console.log(counter);
+  }, 10000);
+  setTimeout(function(){
+    counter = counter - 2;
+    document.getElementById("timerdisplay").innerHTML = counter;
+    console.log(counter);
+  }, 12000);
+  setTimeout(function(){
+    counter = counter - 2;
+    document.getElementById("timerdisplay").innerHTML = counter;
+    console.log(counter);
+  }, 14000);
+  setTimeout(function(){
+    counter = counter - 2;
+    document.getElementById("timerdisplay").innerHTML = counter;
+    console.log(counter);
+  }, 16000);
+  setTimeout(function(){
+    counter = counter - 2;
+    document.getElementById("timerdisplay").innerHTML = counter;
+    console.log(counter);
+  }, 18000);
+  setTimeout(function(){
+    counter = counter - 2;
+    document.getElementById("timerdisplay").innerHTML = "Fire!";
+    console.log(counter);
+  }, 20000);
+}
