b8fcddcf5b2fc111139d5e6612d9f53a47545161
hedyl2 <100638849+hedyl2@users.noreply.github.com>
2022-06-05 23:33:26 -0700

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -6,13 +6,13 @@ function timerdisplayv1(){
     for (let i = counter; i >= 0; i = i - 5)
     {
       const j = i;
-      if (j!= 0)
-      {
-      // change timer back to 1000 when finished testing
-        setTimeout(function(){document.getElementById("timerdisplay").innerHTML = j; console.log(j);}, ((counter - j)*10));
-      }
-      else {
-        setTimeout(function(){document.getElementById("timerdisplay").innerHTML = "Blastoff!"; console.log(j);}, ((counter - j)*10));
-    }
+      if (j != 0 && j > 25)
+        {
+            setTimeout(function(){document.getElementById("timerdisplay").innerHTML = "Time left = " + j; console.log(j);}, ((counter - j)*1000));
+        } else if (j != 0 && j <= 25){
+            setTimeout(function(){document.getElementById("timerdisplay").innerHTML = "Warning Less than half way to launch, time left = " + j; console.log(j);}, ((counter - j)*1000));
+        } else {
+            setTimeout(function(){document.getElementById("timerdisplay").innerHTML = "Blastoff!"; console.log(j);}, ((counter - j)*1000));
+        }
   }
 }
