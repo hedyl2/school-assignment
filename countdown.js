e505dd58dbc64793dd36ac54b5227460fd239b1a
hedyl2 <100638849+hedyl2@users.noreply.github.com>
2022-06-05 23:22:03 -0700

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,56 +1,18 @@
 function timerdisplayv1(){
-	console.log("timerdisplayv1() begin");
-	var counter = 50;
+    console.log("timerdisplayv1() begin");
+
+    var counter = 50;
     document.getElementById("timerdisplay").innerHTML = counter;
-    console.log(counter);
-  setTimeout(function(){
-    counter = counter - 5;
-    document.getElementById("timerdisplay").innerHTML = counter;
-    console.log(counter);
-  }, 5000);
-  setTimeout(function(){
-    counter = counter - 5;
-    document.getElementById("timerdisplay").innerHTML = counter;
-    console.log(counter);
-  }, 10000);
-  setTimeout(function(){
-    counter = counter - 5;
-    document.getElementById("timerdisplay").innerHTML = counter;
-    console.log(counter);
-  }, 15000);
-  setTimeout(function(){
-    counter = counter - 5;
-    document.getElementById("timerdisplay").innerHTML = counter;
-    console.log(counter);
-  }, 20000);
-  setTimeout(function(){
-    counter = counter - 5;
-    document.getElementById("timerdisplay").innerHTML = counter;
-    console.log(counter);
-  }, 25000);
-  setTimeout(function(){
-    counter = counter - 5;
-    document.getElementById("timerdisplay").innerHTML = counter;
-    console.log(counter);
-  }, 30000);
-  setTimeout(function(){
-    counter = counter - 5;
-    document.getElementById("timerdisplay").innerHTML = counter;
-    console.log(counter);
-  }, 35000);
-  setTimeout(function(){
-    counter = counter - 5;
-    document.getElementById("timerdisplay").innerHTML = counter;
-    console.log(counter);
-  }, 40000);
-  setTimeout(function(){
-    counter = counter - 5;
-    document.getElementById("timerdisplay").innerHTML = counter;
-    console.log(counter);
-  }, 45000);
-  setTimeout(function(){
-    counter = counter - 5;
-    document.getElementById("timerdisplay").innerHTML = "Fire!";
-    console.log(counter);
-  }, 50000);
+    for (let i = counter; i >= 0; i = i - 5)
+    {
+      const j = i;
+      if (j!= 0)
+      {
+      // change timer back to 1000 when finished testing
+        setTimeout(function(){document.getElementById("timerdisplay").innerHTML = j; console.log(j);}, ((counter - j)*10));
+      }
+      else {
+        setTimeout(function(){document.getElementById("timerdisplay").innerHTML = "Blastoff!"; console.log(j);}, ((counter - j)*10));
+    }
+  }
 }
